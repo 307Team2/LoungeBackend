662b2f2e9f986e8fb72d0acc961f63e77b48d388
Benjamin Alderfer <Ben@Benjamins-MBP.lfytina2.metronetinc.com>
2015-10-02 14:12:55 -0400

    Integrate grunt, sass, layouts and initial landing page

--- views/layout.jsx
+++ views/layout.jsx
@@ -1,9 +1,36 @@
 var React = require('react');
+var Navigation = require('./common/navigation.jsx');
 
-var HelloMessage = React.createClass({
+var DefaultLayout = React.createClass({
+  getNavigation: function() {
+    if (this.props.page !== "homepage") {
+      return <Navigation/>;
+    }
+  },
+
   render: function() {
-    return <div>Layout?</div>;
+    return (
+      <html lang="en">
+        <head>
+          <meta charset="utf-8"/>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+          <title>{this.props.title}</title>
+
+          <link href='https://fonts.googleapis.com/css?family=Abril+Fatface|Source+Sans+Pro:400,700,300,300italic,400italic,700italic' rel='stylesheet' type='text/css'/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+          <link rel="stylesheet" href="/stylesheets/application.css"/>
+        </head>
+        <body className={this.props.page}>
+          {this.getNavigation()}
+          {this.props.children}
+        </body>
+      </html>
+    );
   }
 });
 
-module.exports = HelloMessage;
+module.exports = DefaultLayout;
