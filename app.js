6c6f18efbb8a0d00cb550122984374ab61f55ac2
Spencer Brown <me@spencer.sx>
2015-10-15 19:57:38 -0400

    Removing users route + reference to it

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/account/', account);
 app.use('/feed/', feed);
 
