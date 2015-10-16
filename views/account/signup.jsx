2c5a77d85a2a02c21c71d5899c0d1b7d501972a6
Spencer Brown <me@spencer.sx>
2015-10-16 18:59:30 -0400

    Update account creation form with new fields

--- views/account/signup.jsx
+++ views/account/signup.jsx
@@ -9,7 +9,17 @@ var SignUp = React.createClass({
           <div className="content">
             <h1>Join Lounge today.</h1>
             <form action="/account/signup" method="post">
-              <input type="text" name="name" placeholder="Full Name" />
+              <input type="text" name="firstName" placeholder="First Name" />
+              <br/>
+              <input type="text" name="lastName" placeholder="Last Name" />
+              <br/>
+              <input type="number" name="age" placeholder="Age" />
+              <br/>
+              <input type="text" name="location" placeholder="Location" />
+              <br/>
+              <input type="text" name="organization" placeholder="Organization" />
+              <br/>
+              <input type="text" name="jobTitle" placeholder="Job Title" />
               <br/>
               <input type="text" name="email" placeholder="Email" />
               <br/>
