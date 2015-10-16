a62201d50d78df043b0139f0c43807357b08b1c5
Roy Fu <royfu@purdue.edu>
2015-10-15 21:37:39 -0400

    Change event link to be name only

--- views/events/event_panel.jsx
+++ views/events/event_panel.jsx
@@ -3,13 +3,11 @@ var React = require('react');
 var EventPanel = React.createClass({
   render: function() {
     return (
-      <a href={"/eventTest"}>
-        <div className="event-panel">
-          <h1>{this.props.event.name}</h1>
-          <h2>{this.props.event.date.getDate()}</h2>
-          <p>{this.props.event.description}</p>
-        </div>
-      </a>
+      <div className="event-panel">
+        <a href={"/eventTest"}><h1>{this.props.event.name}</h1></a>
+        <h2>{this.props.event.date.getDate()}</h2>
+        <p>{this.props.event.description}</p>
+      </div>
     );
   }
 });
