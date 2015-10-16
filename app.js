0f71f98795c396158bb3e20f0a5216dd972b724a
Spencer Brown <me@spencer.sx>
2015-10-15 20:10:08 -0400

    Remove additional reference to users routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var account = require('./routes/account');
 var feed = require('./routes/feed');
 
