617951b04d60d43923eff21a7c8dc35e319e98a6
Spencer Brown <me@spencer.sx>
2015-10-16 18:59:57 -0400

    Update profile view so that it works

--- views/profile.jsx
+++ views/profile.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var ProfileWrapper = require('./layout.jsx');
-var Posts = require('./feed/posts.jsx');
 
 var Profile = React.createClass({
   render: function() {
@@ -10,17 +9,13 @@ var Profile = React.createClass({
           <div className="row">
             <div className="col-sm-4 col-sm-offset-2">
               <div className="panel">
-                <img className="img-thumbnail img-responsive" src={"/images/profile.png"}/> 
-                <h2>{this.props.user.firstname} {this.props.user.lastname}</h2>
+                <h2>{this.props.user.firstName} {this.props.user.lastName}</h2>
                 <p>Age: {this.props.user.age}</p>
                 <p>Location: {this.props.user.location}</p>
                 <p>Organization: {this.props.user.organization}</p>
-                <p>Job Title: {this.props.user.title}</p>
+                <p>Job Title: {this.props.user.jobTitle}</p>
                 <a href="/profile/edit"><button type="button" className="btn btn-default">Edit Profile</button></a>
               </div>
-            </div>
-            <div className="col-sm-4">
-              <Posts posts={this.props.posts} />
             </div>
           </div>
         </div>
