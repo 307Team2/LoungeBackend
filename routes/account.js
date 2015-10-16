be8e18f61a607b85ccb77217b4b84e093cc478c7
Spencer Brown <me@spencer.sx>
2015-10-16 00:15:45 -0400

    Prepend `/account` to account routes

--- routes/account.js
+++ routes/account.js
@@ -1,32 +1,32 @@
 module.exports = function(app) {
 
     /* GET sign up. */
-    app.get('/login', function(req, res, next) {
+    app.get('/account/login', function(req, res, next) {
         res.render('account/login');
     });
 
     /* GET sign up. */
-    app.get('/signup', function(req, res, next) {
+    app.get('/account/signup', function(req, res, next) {
         res.render('account/signup');
     });
 
     /* GET begin password reset page. */
-    app.get('/begin_password_reset', function(req, res, next) {
+    app.get('/account/begin_password_reset', function(req, res, next) {
         res.render('account/begin-password-reset');
     });
 
     /* GET password reset complete page. */
-    app.get('/password_reset_complete', function(req, res, next) {
+    app.get('/account/password_reset_complete', function(req, res, next) {
         res.render('account/password-reset-complete');
     });
 
     /* GET reset email sent page. */
-    app.get('/reset_email_sent', function(req, res, next) {
+    app.get('/account/reset_email_sent', function(req, res, next) {
         res.render('account/reset-email-sent');
     });
 
     /* GET reset password page. */
-    app.get('/reset_password', function(req, res, next) {
+    app.get('/account/reset_password', function(req, res, next) {
         res.render('account/reset-password');
     });
 };
