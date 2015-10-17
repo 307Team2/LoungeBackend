0b6c7983f80cb5a63e77cb5226359c7d2e7645d9
Spencer Brown <me@spencer.sx>
2015-10-16 21:24:29 -0400

    Update view to use form values instead of placeholders - BROKEN

--- views/edit-profile.jsx
+++ views/edit-profile.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var ProfileWrapper = require('./layout.jsx');
-var Posts = require('./feed/posts.jsx');
 
 var EditProfile = React.createClass({
   render: function() {
@@ -10,29 +9,25 @@ var EditProfile = React.createClass({
           <div className="row">
             <div className="col-sm-4 col-sm-offset-2">
               <div className="panel">
-                <img className="img-thumbnail img-responsive" src={"/images/profile.png"}/> 
-                <h2>{this.props.user.firstname} {this.props.user.lastname}</h2>
+                <h2>{this.props.user.firstName} {this.props.user.lastName}</h2>
                 <p>
                   Age:
-                  <input type="text" placeholder={this.props.user.age} />
+                  <input type="text" value={this.props.user.age} />
                 </p>
                 <p>
                   Location:
-                  <input type="text" placeholder={this.props.user.location} />
+                  <input type="text" value={this.props.user.location} />
                 </p>
                 <p>
                   Organization:
-                  <input type="text" placeholder={this.props.user.organization} />
+                  <input type="text" value={this.props.user.organization} />
                 </p>
                 <p>
                   Job Title:
-                  <input type="text" placeholder={this.props.user.title} />
+                  <input type="text" value={this.props.user.title} />
                 </p>
                 <a href="/profile"><button type="button" className="btn btn-success">Submit Changes</button></a>
               </div>
-            </div>
-            <div className="col-sm-4">
-              <Posts posts={this.props.posts} />
             </div>
           </div>
         </div>
