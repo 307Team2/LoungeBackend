de752cbd25e8031bd4ac484b15570a85d3d050bc
Benjamin Alderfer <Ben@Benjamins-MBP.lfytina2.metronetinc.com>
2015-11-09 12:12:28 -0500

    Route modifications for feed and profile

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,8 @@
+var jwt = require('jsonwebtoken');
+var async = require('async');
 var Post = require('../models/post.js');
-var moment = require('moment');
+var User = require('../models/user.js');
+var postServices = require('../services/postServices');
 
 module.exports = function(app) {
 
@@ -10,33 +13,66 @@ module.exports = function(app) {
     // }
     app.post('/posts/create', function(req, res, next) {
 
-        // FIXME: Use req.user once authentication is implemented in order to set authorId
-        postServices.createPost(req.body, function(err, post) {
-            if (err) {
-                console.log(err);
+        var auth_token = req.get('Authorization');
 
-                // TODO: Render template here w/ error message
-                res.sendStatus(500);
-            } else {
-                console.log('New post created: ' + event);
+        jwt.verify(auth_token, app.get('superSecret'), function(error, user) {
+        
+            if (error) {
+        
+                res.status(500).send(error);
+        
+            } else if (user) {
+        
+                req.body.authorId = user._id;
+                postServices.createPost(req.body, function(err, post) {
+                    if (err) {
+                        console.log(err);
 
-                // TODO: Render template here
-                res.sendStatus(201);
+                        // TODO: Render template here w/ error message
+                        res.sendStatus(500);
+                    } else {
+                        console.log('New post created: ' + post);
+
+                        // TODO: Render template here
+                        res.sendStatus(201);
+                    }
+                });
+        
             }
+        
         });
+
     });
 
-    app.get('/posts/all', function(req, res, next) {
+    app.get('/posts/all?', function(req, res, next) {
+
+        var limit = req.query.limit;
+        var lastTimestamp = req.query.lastTimestamp;
 
         // might want to limit posts to certain time period
-        postServices.findAllPosts(function(err, posts) {
+        postServices.findAllPosts(limit, lastTimestamp, function(err, posts) {
+            if (err) {
+                res.status(500).send(err);
+            }
 
-            var templateData = {
-                title: 'Lounge',
-                posts: posts
-            };
+            var expandPost = function(post, cb) {
+                User.findById(post.authorId, function(error, author) {
+                    var newPost = post.toObject();
+                    if (author) {
+                        newPost.displayName = author.firstName + " " + author.lastName;
+                    } else {
+                        newPost.displayName = "Anonymous"
+                    }
+                    cb(null, newPost);
+                });
+            }
 
-            res.render('feed/index', templateData);
+            async.map(posts, expandPost, function(error, expandedPosts) {
+                res.json({
+                    posts: expandedPosts
+                });
+            });
+
         });
     });
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,46 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var Post = require('../models/post');
 
 module.exports = function(app) {
   
   /* GET profile page. */
-  app.get('/profile/', function(req, res, next) {
-    res.render('profile', {
-      user: {
-        firstname: "Miranda",
-        lastname: "Mott",
-        age: 21,
-        location: "West Lafayette, IN",
-        organization: "ITaP",
-        title: "Web/Mobile App Intern"
-      },
-      posts: [
-        {
-          content: "This is a test post",
-          timestamp: Date.now(),
-          author: {
-            firstname: "Miranda",
-            lastname: "Mott"
-          }
-        },
-        {
-          content: "WUBBALUBBADUBDUB",
-          timestamp: Date.now(),
-          author: {
-            firstname: "Miranda",
-            lastname: "Mott"
-          }
-        },
-        {
-          content: "This is another test post",
-          timestamp: Date.now(),
-          author: {
-            firstname: "Miranda",
-            lastname: "Mott"
-          }
-        }
-      ]
+  app.get('/profile/:id', function(req, res, next) {
+ 
+    User.findById(req.params.id, function(error, user) {
+      Post.find({authorId: req.params.id}).sort({createdAt: -1}).exec(function(error, posts) {
+        res.json({
+          user: user,
+          posts: posts
+        });
+      });
     });
+
   });
 
   /* GET edit profile page. */
