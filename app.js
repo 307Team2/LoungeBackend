94b87808e6fbfa1a32d01fb2617314c4ec62994f
Benjamin Alderfer <Ben@Benjamins-MBP.lfytina2.metronetinc.com>
2015-11-09 14:46:08 -0500

    Try app.all

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var url = require('url');
 
 var app = express();
 
-app.use(function (req, res, next) {
+app.all('*', function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', process.env.NODE_ENV === 'production' ? 'http://lounge-web.herokuapp.com' : 'http://localhost:8888');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
