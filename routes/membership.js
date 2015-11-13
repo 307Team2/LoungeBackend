cd656095c76eec6dc1c37d70bd614c873e53ca82
Benjamin Alderfer <Ben@Benjamins-MacBook-Pro.local>
2015-11-13 01:21:21 -0500

    Some stripe stuff

--- routes/membership.js
+++ routes/membership.js
@@ -1,28 +1,10 @@
+var jwt = require('jsonwebtoken');
 
-stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
 module.exports = function(app) {
-
-    app.get('/membership/create', function(req, res, next) {
-        console.log("test");
-        res.render('payment/paywall.jsx');
-    });
-
-
-//making first charge
-var stripe = require("stripe")("sk_test_VgZHXpIF8hRIxSuoobWHnap7");
-
-// (Assuming you're using express - expressjs.com)
-// Get the credit card details submitted by the form
-var stripeToken = request.body.stripeToken;
-
-stripe.customers.create({
-  source: stripeToken,
-  plan: "gold",
-  email: "payinguser@example.com"
-}, function(err, customer) 
 
     app.post('/membership/create', function(req, res, next) {
 
+        var stripe = require("stripe")("sk_test_VgZHXpIF8hRIxSuoobWHnap7");
         // TODO: Verify tier amounts with team
         var tierMapping = {
             'bronze': 10000,
@@ -32,6 +14,14 @@ stripe.customers.create({
 
         var stripeToken = req.body.stripeToken;
         var tier = req.body.tier;
+
+        stripe.customers.create({
+          source: stripeToken,
+          plan: "gold",
+          email: "payinguser@example.com"
+        }, function(err, customer) {
+
+        });
 
         var charge = stripe.charges.create({
             amount: tierMapping[tier],
@@ -45,23 +35,23 @@ stripe.customers.create({
             }
         });
     });
+
+    //to view one member's membership 
+    app.get('/member/:id', function(req, res, next) {        
+        if(req.user)    {
+            res.send(req.user.tier);
+            } else {
+                res.sendStatus(401);
+            }
+        });
+
+    //cancel subscription 
+    stripe.customers.cancelSubscription(
+      "cus_3R1W8PG2DmsmM9",
+      "sub_3R3PlB2YlJe84a",
+      function(err, confirmation) {
+        // asynchronously called
+      }
+    );
+
 };
-
-
-//to view one member's membership 
-app.get('/member/:id', function(req, res, next) {        
-    if(req.user)    {
-        res.send(req.user.tier);
-        } else {
-            res.sendStatus(401);
-        }
-    });
-
-//cancel subscription 
-stripe.customers.cancelSubscription(
-  "cus_3R1W8PG2DmsmM9",
-  "sub_3R3PlB2YlJe84a",
-  function(err, confirmation) {
-    // asynchronously called
-  }
-);
